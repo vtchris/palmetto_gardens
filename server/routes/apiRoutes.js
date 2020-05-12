ff52361f589b1890e5451319a13180236bbad875
VTC <vtchris@hotmail.com>
2020-05-12 19:35:39 -0400

    API update to get category list to order page

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const productsController = require("../controllers/productController");
+const categoryController = require("../controllers/categoryController")
 
 router.route("/products/active")
     .get(productsController.findActive)
 
+router.route("/categories")
+    .get(categoryController.findAll)
 
 module.exports = router;
