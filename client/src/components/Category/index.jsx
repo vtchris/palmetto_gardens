7726ab92e918fe3c06459cd49f876bb079d4419e
VTC <vtchris@hotmail.com>
2020-05-14 15:24:09 -0400

    Selecting category displays products available.

---
+++ client/src/components/Category/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Category({ category, id, img, onClick }) {
+  return (
+    <div className="card bg-light m-2" data-id={id} onClick={onClick}>
+      
+      <div className="card-header bg-secondary text-white" data-id={id}>
+      <h5 className="card-title" data-id={id}>{category}</h5>
+      </div>
+      <div className="card-body" data-id={id}>
+        <img className="img-fluid" src={img} data-id={id}/>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
--- client/src/components/Product/index.jsx
+++ client/src/components/Product/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Product(props) {
   return (
-    <div className="card border-secondary mb-3">
+    <div className="card border-secondary m-2">
       {/* <div className="card-header"></div> */}
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        <p>
-          {props.price} {props.unit}
-        </p>
+        <p>{`$${props.price.toFixed(2)} per ${props.unit}`}</p>
+      </div>
+      <div className="card-footer">
         <button className="btn btn-lg btn-success">
           <span className="fas fa-cart-plus mr-3"></span> Add to Cart
         </button>
