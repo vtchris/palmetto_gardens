91bbb642daad827754ea6255d5febf724bdddc58
VTC <vtchris@hotmail.com>
2020-05-30 16:09:25 -0400

    Article component/card start

---
+++ client/src/components/Article/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Article(props) {
+  return (
+    <div className="col col-md-6 col-12">
+      <article className="card m-3">
+        <header className="card-header bg-secondary text-light">
+          <h1 className="card-title">{props.title.toUpperCase()}</h1>
+        </header>
+        <main className="card-body">
+          {props.content.map((p) => (
+            <p>{p}</p>
+          ))}
+        </main>
+      </article>
+    </div>
+  );
+}
+
+export default Article;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import Article from "../components/Article";
 
-function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+class Home extends Component {
+  state = {
+    articles: [],
+  };
+  componentDidMount() {
+    const newState = this.state;
+    newState.articles.push({
+      title: "test",
+      content: ["Paragraph1", "Paragraph2. Second Sentence."],
+    });
+    newState.articles.push({
+      title: "test2",
+      content: [
+        "Paragraph1. Sentence 2",
+        "Paragraph2. Second Sentence. Sentence 3.",
+      ],
+    });
+
+    this.setState(newState);
+  }
+  render() {
+    return (
+      <>
+        <h1>Home</h1>
+        <main className="container">
+          <div className="row">
+            {this.state.articles.map((article) => (
+              <Article
+                title={article.title}
+                content={article.content}
+              ></Article>
+            ))}
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default Home;
