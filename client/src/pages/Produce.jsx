f8c1a1c4fc11aef4b3f9da98c792c17fa2ffa702
VTC <vtchris@hotmail.com>
2020-06-01 13:55:24 -0400

    Add articles to produce and services pages.

--- client/src/pages/Produce.jsx
+++ client/src/pages/Produce.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../utils/API";
+import Article from "../components/Article";
 
-function Produce() {
-  return (
-    <>
-      <h1>Produce</h1>
-    </>
-  );
+class Produce extends Component {
+  state = {
+    articles: [],
+  };
+  componentDidMount() {
+    const newState = this.state;
+
+    API.getArticles()
+      .then((res) => {
+        newState.articles = res.data.filter(
+          ({ active, category }) => active && [3].includes(category)
+        );
+        newState.articles.forEach(article => {
+          article.content = article.content
+                                    .split('.')
+                                    .map(sentence =>  sentence.trim())
+                                    .filter(sentence => sentence.length > 0);
+        })
+       
+        console.log(newState);
+        this.setState(newState);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Produce</h1>
+        <main className="container">
+          <div className="row">
+            {this.state.articles.map((article) => (
+              <Article
+                key={article.id}
+                title={article.title}
+                category={article.category}
+                content={article.content}
+              ></Article>
+            ))}
+          </div>
+        </main>
+      </>
+    );
+
+  }
+  
 }
 
 export default Produce;
