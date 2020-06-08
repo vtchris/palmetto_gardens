42f9a1f8edab4f45f15eefaecbe469a5b28a5140
VTC <vtchris@hotmail.com>
2020-06-08 13:08:52 -0400

    Added thank you email template, fixed phone number validation

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASSWORD
     }
 });
-sendHtml = (mailOptions) => {
+sendContact = (mailOptions) => {
     fs.readFile(__dirname + '/../emailTemplates/inquiry.html', 'utf8', function(err,HTML){
         if(err) console.log(err);
         
@@ -17,6 +17,34 @@ sendHtml = (mailOptions) => {
         HTML = HTML.replace(/{NAME}/i,`${mailOptions.firstName} ${mailOptions.lastName}` )
         HTML = HTML.replace(/{PHONE}/i,mailOptions.phone)
         HTML = HTML.replace(/{MESSAGE}/i,mailOptions.text)
+        mailOptions.html = HTML;
+     
+        sendEmail(mailOptions)
+        sendThankyou(mailOptions);
+    })
+
+}
+sendThankyou = (mailOptions) => {
+    // Flip the to and from email addresses for the thank you
+    const thankYouOptions = mailOptions;
+    thankYouOptions.from = mailOptions.to;
+    thankYouOptions.to = mailOptions.from;
+    if(thankYouOptions.subject = "General Question") {thankYouOptions.subject = "Thank you"}
+    mailOptions = thankYouOptions;
+    const company = mailOptions.company;
+
+    fs.readFile(__dirname + '/../emailTemplates/thankyou.html', 'utf8', function(err,HTML){
+        if(err) console.log(err);
+              
+        HTML = HTML.replace(/{COMPANY}/ig,company.companyName)
+        HTML = HTML.replace(/{ADDRESS1}/ig,company.address1)
+        HTML = HTML.replace(/{ADDRESS2}/ig,
+            company.address2
+            ?  `<tr><td style="text-align:center;">${company.address2}</td></tr>`
+            :  "")        
+        HTML = HTML.replace(/{CSZ}/i,`${company.city}, ${company.state} ${company.zipCode}` )
+        HTML = HTML.replace(/{PHONE}/i,company.phone1)
+        
         mailOptions.html = HTML;
      
         sendEmail(mailOptions)
@@ -33,10 +61,10 @@ sendEmail = (mailOptions) => {
         }
     })
 }
-module.exports = {
+module.exports = {   
     send: function (req,res) {
         const mailOptions = req.body;
-        sendHtml(mailOptions);      
+        sendContact(mailOptions);      
         res.status(200).json("Email sent");
         
     }
--- server/controllers/settingController.js
+++ server/controllers/settingController.js
@@ -2,7 +2,7 @@ const db = require("../models");
 
 module.exports = {
     findAll: function (req, res) {
-
+        
         db.Settings
             .findAll({ })
             .then(data => res.json(data))
